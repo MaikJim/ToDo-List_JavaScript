d871c4ef3f98e10c266729142dd600e724978d0c
MaikJim <maikoljimenez427@gmail.com>
2023-06-22 14:54:22 -0400

    feat: :green_heart: finish the project

--- app.js
+++ app.js
@@ -1,63 +1,90 @@
-const formulario = document.getElementById('formulario');
-const listaTarea = document.getElementById('lista-tareas');
+const form = document.getElementById('formulario');
+const tasksList = document.getElementById('lista-tareas');
 const template = document.getElementById('template').content;
 const input = document.getElementById('input');
 const fragment = document.createDocumentFragment();
-let tareas = {};
+let tasks = {};
 
 document.addEventListener('DOMContentLoaded', () => {
-  pintarTareas();
+  if(localStorage.getItem('tasks')){
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  taskPaint();
 });
 
-listaTarea.addEventListener('click', (e) => {
-  btnAccion(e);
-});
+tasksList.addEventListener('click', (e) => {btnAction(e);});
 
-formulario.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  setTarea();
+  setTask(e);
 });
 
-const setTarea = (e) => {
+const setTask = (e) => {
   if (input.value.trim() === '') {
     console.log('Esta vacio');
     return;
   }
-  const tarea = {
+  const task = {
     id: Date.now(),
-    texto: input.value,
-    estado: false,
+    text: input.value,
+    state: false,
   };
-  tareas[tarea.id] = tarea;
+  tasks[task.id] = task;
 
-  formulario.reset();
+  form.reset();
   input.focus();
 
-  pintarTareas();
+  taskPaint();
 };
 
-const pintarTareas = () => {
-  listaTarea.innerHTML = '';
-  Object.values(tareas).forEach((tarea) => {
+const taskPaint= () => {
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
+  if(Object.values(tasks).length === 0){
+    tasksList.innerHTML = `
+    <div id="lista-tareas" class="mt-2">
+        <div class="alert alert-dark">Sin tareas pendientes 😍</div>
+      </div>`
+    return
+  }
+
+  tasksList.innerHTML = '';
+  Object.values(tasks).forEach((task) => {
     const clone = template.cloneNode(true);
-    clone.querySelector('p').textContent = tarea.texto;
-    clone.querySelectorAll('.fas')[0].dataset.id = tarea.id;
+    clone.querySelector('p').textContent = task.text;
+
+    if(task.state){
+      clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
+      clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle','fa-undo-alt' )
+      clone.querySelector('p').style.textDecoration = 'line-through'
+    }
+
+
+    clone.querySelectorAll('.fas')[0].dataset.id = task.id;
     fragment.appendChild(clone);
   });
-  listaTarea.appendChild(fragment);
+  tasksList.appendChild(fragment);
 };
 
-const btnAccion = (e) => {
+const btnAction = e => {
+  console.log(e.target.classList.contains('fa-check-circle'))
   if (e.target.classList.contains('fa-check-circle')) {
-    console.log(e.target.dataset.id);
-    tareas[e.target.dataset.id].estado = true;
-    pintarTareas();
+      tasks[e.target.dataset.id].state = true
+      taskPaint()
   }
 
   if (e.target.classList.contains('fa-minus-circle')) {
-    delete tareas[e.target.dataset.id];
-    pintarTareas();
-    console.log(tareas);
+      // console.log(e.target.dataset.id)
+      delete tasks[e.target.dataset.id]
+      taskPaint()
   }
-  e.stopPropagation();
-};
+
+  if (e.target.classList.contains('fa-undo-alt')) {
+      tasks[e.target.dataset.id].state = false
+      taskPaint()
+  }
+
+  e.stopPropagation()
+}
